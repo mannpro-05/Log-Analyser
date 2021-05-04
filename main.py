f642707e2aab2b50eec24a236e9d645325ef37bd
mannpro-05 <mannprajapati567@gmail.com>
2021-05-04 15:26:41 +0530

    fifteenth-commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sqlite3 as sl
 import time
 import datetime
-start = time.time()
+import os
 conn = sl.connect('logs.db')
+start = time.time()
+os.chdir('/var/log/safesquid/extended/')
 mapper = {
     "DATE_TIME": 3,
     "STATUS": 5,
@@ -40,60 +42,56 @@ months = {
     "Dec": 12
 }
 
-def line_split(row):
-    lst = row.split('\t')
-    return lst
 
 exception = ['STATUS', 'UPLOAD', 'DOWNLOAD']
 counter = 0
-with open('small.log','r') as logs:
-    for i in logs:
-        lst = line_split(i)
-        finalData = []
-        counter += 1
-        if len(lst) == 1 or 'date_time' in lst[3].strip('"'):
-            continue
-        try:
-            for key, val in mapper.items():
-                if key in exception:
-                    finalData.append(lst[val].strip('"'))
-                    continue
-                elif key == 'DATE_TIME':
-                    date = lst[val].strip('"').split(':')
-                    day = date[0].split('/')
-                    day[1] = months[day[1]]
-                    finalData.append(
-                        datetime.datetime(int(day[2]), int(day[1]), int(day[0]), int(date[1]), int(date[2]),
-                                          int(date[3])).timestamp())
-                    continue
-                elif key == 'URL':
-                    if lst[val].strip('"') != '-':
-                        lst[val] = lst[val].split('/')[2].split(':')[0].strip('"')
-                elif key == 'USERNAME':
-                    lst[val] = lst[val].split('@')[0].strip('"')
+for log in os.listdir():
+    with open(log,'r') as logs:
+        for i in logs:
+            lst = i.split('\t')
+            finalData = []
+            counter += 1
+            if len(lst) == 1 or 'date_time' in lst[3].strip('"'):
+                continue
+            try:
+                for key, val in mapper.items():
+                    if key in exception:
+                        finalData.append(lst[val].strip('"'))
+                        continue
+                    elif key == 'DATE_TIME':
+                        date = lst[val].strip('"').split(':')
+                        day = date[0].split('/')
+                        day[1] = months[day[1]]
+                        finalData.append(
+                            datetime.datetime(int(day[2]), int(day[1]), int(day[0]), int(date[1]), int(date[2]),
+                                              int(date[3])).timestamp())
+                        continue
+                    elif key == 'URL':
+                        if lst[val].strip('"') != '-':
+                            lst[val] = lst[val].split('/')[2].split(':')[0].strip('"')
+                    elif key == 'USERNAME':
+                        lst[val] = lst[val].split('@')[0].strip('"')
 
-                cursor = conn.execute("SELECT ID FROM %s WHERE %s = ?" % (key, key), (lst[val].strip('"'),))
-                value = cursor.fetchone()
-                if value == None:
-                    id = conn.execute("SELECT count(ID) FROM %s" % (key))
-                    id = id.fetchone()[0] + 1
-                    finalData.append(id)
-                    conn.execute("INSERT INTO %s VALUES(?,?)" % (key), (id, lst[val].strip('"')))
-                else:
-                    finalData.append(value[0])
-            final = tuple(finalData)
-            conn.execute("INSERT INTO FINAL_LOG VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", final)
-            if counter % 100000 == 0:
-                print(counter / 100000)
-                conn.commit()
-        except Exception as e:
-            print(e)
-        
-    conn.commit()
+                    cursor = conn.execute("SELECT ID FROM %s WHERE %s = ?" % (key, key), (lst[val].strip('"'),))
+                    value = cursor.fetchone()
+                    if value == None:
+                        id = conn.execute("SELECT count(ID) FROM %s" % (key))
+                        id = id.fetchone()[0] + 1
+                        finalData.append(id)
+                        conn.execute("INSERT INTO %s VALUES(?,?)" % (key), (id, lst[val].strip('"')))
+                    else:
+                        finalData.append(value[0])
+                final = tuple(finalData)
+                conn.execute("INSERT INTO FINAL_LOG VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", final)
+                if counter % 100000 == 0:
+                    print(counter / 100000)
+                    conn.commit()
+            except Exception as e:
+                print(e)
+
+        conn.commit()
 
 
 
 end = time.time()
-#conn.execute("INSERT INTO INSERT_TIME VALUES (?,?)",('shorten_logs',end-start))
-#conn.commit()
 print(end - start)
---
+++ updateDatabase.py
@@ -0,0 +1,15 @@
+import os
+import time
+import insertRecords
+os.chdir('/var/log/safesquid/extended/')
+
+cronTime =int(time.time())
+
+for i in os.listdir():
+    mTime = os.path.getmtime(i)
+    cTime = os.path.getctime(i)
+    if (cronTime >= int(cTime) and (cronTime - 900) <= int(cTime)):
+        insertRecords.insertRecords(i)
+
+    elif (cronTime>= int(mTime) and (cronTime - 900) <= int(mTime)):
+        insertRecords.insertModifiedRecordsRecords(i,cronTime - 900)
