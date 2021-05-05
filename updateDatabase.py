a3c909cccd10d9582649445816c6ab8c56483e2d
mannpro-05 <mannprajapati567@gmail.com>
2021-05-05 14:19:29 +0530

    twentyfirst-commit

--- updateDatabase.py
+++ updateDatabase.py
@@ -10,7 +10,9 @@ for i in os.listdir():
     mTime = os.path.getmtime(i)
     cTime = os.path.getctime(i)
     if (cronTime >= int(cTime) and (cronTime - 900) <= int(cTime)):
+        print(i, cTime)
         insertRecords.insertRecords(i)
 
     elif (cronTime>= int(mTime) and (cronTime - 900) <= int(mTime)):
+        print(i, mTime)
         insertRecords.insertModifiedRecordsRecords(i,cronTime - 900)
