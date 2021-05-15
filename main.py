d6287abd7fc7a3799d7867fb447992bc3b9bf7bb
mannpro-05 <mannprajapati567@gmail.com>
2021-05-15 12:45:58 +0530

    twentyninth(UpdateDatabase)-commit

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ exception = ['STATUS', 'UPLOAD', 'DOWNLOAD']
 max = int(os.listdir()[0].split('-')[0])
 for log in os.listdir():
     counter = 0
-    print(log.split('.')[-1])
     if log == 'extended.log' or log.split('.')[-1] != 'log':
         continue
     with open(log,'r') as logs:
